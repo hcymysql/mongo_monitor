05614dd584211898f6a3e2b9aaa47a162b30b986
hcymysql <chunyang_he@139.com>
2020-05-11 14:20:59 +0800

    Update mongo_monitor_schema.sql

--- schema_sql/mongo_monitor_schema.sql
+++ schema_sql/mongo_monitor_schema.sql
@@ -12,9 +12,9 @@ MySQL - 8.0.19 : Database - mongo_monitor
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`iqj_mongo_monitor` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`mongo_monitor` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
-USE `iqj_mongo_monitor`;
+USE `mongo_monitor`;
 
 /*Table structure for table `mongo_repl_status` */
 
